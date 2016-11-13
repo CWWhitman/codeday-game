1005cb5c6fc7de4024a17b62a3092455ec7b5417
tommy-mor <thmorriss@gmail.com>
2016-11-13 01:22:40 -0800

    made level better

--- level.py
+++ level.py
@@ -1,7 +1,9 @@
 print "test"
 l = []
 l.append("".join(["1" for _ in range(30)]))
-for a in range(13):
+for a in range(10):
     l.append("".join(["0" for _ in range(25)]) +"11111")
+for a in range(3):
+    l.append("".join(["0" for _ in range(10)]) +"1" * 20)
 l.append("".join(["1" for _ in range(30)]))
 
